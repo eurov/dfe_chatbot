be7c3f543d486c7d7ed1951409844382d21260e2
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-13 20:41:11 +0400

    feat: add a function to output <Help> hint
    The function overwrites the bot' response by adding a hint at the user's request

--- app_tools.py
+++ app_tools.py
@@ -1,3 +1,5 @@
+import re
+
 from df_engine.core import Context, Actor
 from app_vocabulary import home_page_description, help_description
 
@@ -6,4 +8,12 @@ def home_page(ctx: Context, actor: Actor, *args, **kwargs):
     """Returns the description of home page"""
     return home_page_description
 
+
+def show_help(ctx: Context, actor: Actor, *args, **kwargs):
+    """Returns a hint with useful information"""
+    if re.search(r'(?i)(help)', ctx.last_request):
+        processed_node = ctx.current_node
+        processed_node.response = f"{ctx.last_response} \033[36m{help_description}\033[32m"
+        ctx.overwrite_current_node_in_processing(processed_node)
+    return ctx
 
