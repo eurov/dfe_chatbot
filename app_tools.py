e55c7654f3a5e3e54b7c5b9da29ed6dbe4a23d25
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-14 22:31:15 +0400

    fix: upgrade custom_transfer and custom_response function
    For transfer - to be able to process the user's request after help|history call.
    For response - to overwrite a ctx only if the condition is met.

--- app_tools.py
+++ app_tools.py
@@ -13,15 +13,17 @@ def home_page(ctx: Context, actor: Actor, *args, **kwargs):
 
 def get_help(ctx: Context, actor: Actor, *args, **kwargs):
     """Returns help hint"""
-    if re.search(r'(?i)(help)', ctx.last_request):
-        ctx.current_node.response = f"{ctx.last_response} \033[36m{help_description}\033[32m"
+    if re.search(r"(?i)(help)", ctx.last_request):
+        ctx.current_node.response = (
+            f"{ctx.last_response} \033[36m{help_description}\033[32m"
+        )
         ctx.overwrite_current_node_in_processing(ctx.current_node)
     return ctx
 
 
 def get_history(ctx: Context, actor: Actor, *args, **kwargs):
     """Returns current session history"""
-    if re.search(r'(?i)(history)', ctx.last_request):
+    if re.search(r"(?i)(history)", ctx.last_request):
         history = json.dumps(ctx.misc, indent=4)
         ctx.current_node.response = f"{ctx.last_response}\n\t\033[36m<Current session history>\n{history}\033[32m"
         ctx.overwrite_current_node_in_processing(ctx.current_node)
@@ -32,43 +34,53 @@ def save_session_history(ctx: Context, actor: Actor, *args, **kwargs) -> Context
     """Stores current chat history in ctx.misc"""
     dt = datetime.now()
     dt_string = dt.strftime("Date: %d/%m/%Y  time: %H:%M:%S")
-    ctx.misc[f'{dt_string} :> Me'] = ctx.last_request  # saving a last user request
+    ctx.misc[f"{dt_string} :> Me"] = ctx.last_request  # saving a last user request
     processed_node = ctx.current_node
-    if hasattr(processed_node.response, '__call__'):
+    if hasattr(processed_node.response, "__call__"):
         value = processed_node.response.__name__
     else:
         value = processed_node.response
-    ctx.misc[f'{dt_string} :> Bot'] = value  # saving a last bot response
+    ctx.misc[f"{dt_string} :> Bot"] = value  # saving a last bot response
     return ctx
 
 
 def custom_response(ctx: Context, actor: Actor, *args, **kwargs):
     """Configuring an operator's response"""
-    section = None
-    if re.search(r'(?i)(model)', ctx.last_request):
-        section = 'Our models'
-    if re.search(r'(?i)(drive)', ctx.last_request):
-        section = 'Sign up for a test drive'
-    ctx.current_node.response = f"Would you like me to transfer you to \033[36m<{section}>\033[32m section?" \
-                                f"\t[Y/n]"
-    ctx.overwrite_current_node_in_processing(ctx.current_node)
+
+    if re.search(r"(?i)(model)", ctx.last_request):
+        section = "Our models"
+        ctx.current_node.response = (
+            f"Would you like me to transfer you to \033[36m<{section}>\033[32m section?"
+            f"\n\t[Y] > confirm transfer"
+            f"\n\t[n] > go back to reception"
+        )
+        ctx.overwrite_current_node_in_processing(ctx.current_node)
+    if re.search(r"(?i)(drive)", ctx.last_request):
+        section = "Sign up for a test drive"
+        ctx.current_node.response = (
+            f"Would you like me to transfer you to \033[36m<{section}>\033[32m section?"
+            f"\n\t[Y] > confirm transfer"
+            f"\n\t[n] > go back to reception"
+        )
+        ctx.overwrite_current_node_in_processing(ctx.current_node)
     return ctx
 
 
 def custom_transfer(ctx: Context, actor: Actor, *args, **kwargs):
     """Returns the NodeLabel2Type depending on request"""
-    if re.search('y', ctx.last_request):
-        if re.search('model', ctx.requests[max(ctx.requests)-1]):
+
+    if re.search("y", ctx.last_request):
+        pre_request = ctx.requests[max(ctx.requests) - 1]
+        if re.search("help|history", pre_request):
+            pre_request = ctx.requests[max(ctx.requests) - 2]
+        if re.search("model", pre_request):
             return "models_flow", "node1", 1.1
-        elif re.search('drive', ctx.requests[max(ctx.requests)-1]):
+        elif re.search("drive", pre_request):
             return "test_drive_flow", "node1", 1.1
-        elif re.search('dealer', ctx.requests[max(ctx.requests)-1]):
+        elif re.search("dealer", pre_request):
             return "dealer_flow", "node1", 1.1
-        elif re.search('service', ctx.requests[max(ctx.requests)-1]):
+        elif re.search("service", pre_request):
             return "service_flow", "service_node", 1.1
-    if re.search('n', ctx.last_request):
+    if re.search("n", ctx.last_request):
         return "operator_flow", "reception_node"
     return "root", "fallback_node", 1.1
-
-
-
