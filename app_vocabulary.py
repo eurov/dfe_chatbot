a64bbff9b98c5316b77f205e9772f212ba53955e
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-14 20:58:21 +0400

    feat: add app vocabulary
    Docstring context will be stored here

---
+++ app_vocabulary.py
@@ -0,0 +1,22 @@
+home_page_description = """
+    ----------------------------------------------------
+    You are welcomed by the online assistant of AVTOVAZ
+    ----------------------------------------------------
+
+    Choose an option:
+
+    [1] > Contact the operator 
+    [2] > Our models
+    [3] > Sign up for a test drive
+    [4] > Find a dealer
+    [5] > Assistance service
+    [help] > Chat navigation
+    """
+
+help_description = """
+
+    <Help>
+    [home] > go to home page
+    [back] > go back to previous step
+    [history] > get the current session history
+    """
