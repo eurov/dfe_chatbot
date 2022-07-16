e4c542df09a60f3ec62511c6c15b8f0fb3742a37
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-16 22:57:10 +0400

    fix: move the test block to separate module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from typing import Optional, Union
+from typing import Union
 from df_engine.core import Context, Actor
 
 from app_scenario import script
@@ -19,21 +19,14 @@ def turn_handler(
     in_request: str,
     ctx: Union[Context, str, dict],
     _actor: Actor,
-    true_out_response: Optional[str] = None,
 ):
     ctx = Context.cast(ctx)
     ctx.add_request(in_request)
     ctx = actor(ctx)
     out_response = ctx.last_response
-
-    # the next condition branching needs for testing
-    if true_out_response is not None and true_out_response != out_response:
-        msg = f"in_request={in_request} -> true_out_response != out_response: {true_out_response} != {out_response}"
-        raise Exception(msg)
-    else:
-        logging.info(
-            "\033[37mBot: \033[3m\033[32m{}\033[37m\n<<hint>> I'm here: {}\033[0m".format(out_response, ctx.last_label)
-        )
+    logging.info(
+        "\033[37mBot: \033[3m\033[32m{}\033[37m\n<<hint>> I'm here: {}\033[0m".format(out_response, ctx.last_label)
+    )
     return out_response, ctx
 
 
