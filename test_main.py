9dcb11b29783469e22fe1094a4af31046713f235
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-18 21:32:02 +0400

    chore: extend test_dialog for testing

--- test_main.py
+++ test_main.py
@@ -1,36 +1,21 @@
 import unittest
+
+from app_vocabulary import home_page_description
 from main import turn_handler, actor
 
 testing_dialog = [
-    (
-        "home",
-        """
-    ----------------------------------------------------
-    You are welcomed by the online assistant of AVTOVAZ
-    ----------------------------------------------------
-
-    Choose an option:
-
-    [1] > Contact the operator 
-    [2] > Our models
-    [3] > Sign up for a test drive
-    [4] > Find a dealer
-    [5] > Assistance service
-    [help] > Chat navigation
-
-    """,
-    ),
-    ("1", "How can I help you?")
-    # ("i'm fine, how are you?", "Good. What do you want to talk about?"),
-    # ("talk about music.", "I love `System of a Down` group, would you like to tell about it? "),
-    # ("yes", "System of a Downis an Armenian-American heavy metal band formed in in 1994."),
-    # ("next", "The band achieved commercial success with the release of five studio albums."),
-    # ("back", "System of a Downis an Armenian-American heavy metal band formed in in 1994."),
-    # ("repeat", "System of a Downis an Armenian-American heavy metal band formed in in 1994."),
-    # ("next", "The band achieved commercial success with the release of five studio albums."),
+    ("start", home_page_description),
+    ("1", "How can I help you?"),
+    ("is it raining?", "Please rephrase your question.."),
+    ("home", home_page_description),
+    ("2", "Choose a model: ....."),
+    ("I heard a lot about Lada Kalina", "Let me present you LADA KALINA....."),
+    ("back", "Choose a model: ....."),
+    ("ferrari", "\033[31mSorry, I couldn't find anything for this query\033[0m"),
 ]
 
 
+# TODO: upgrade test scenario -> process a series of requests and check only final result
 class TestHandler(unittest.TestCase):
     def test_turn_handler(self):
         ctx = {}
