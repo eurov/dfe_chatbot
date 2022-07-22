c74947e27adba6226ab777b08097c2fe84158247
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-23 00:17:22 +0400

    feat: collect all texts for scenario

---
+++ text.py
@@ -0,0 +1,53 @@
+HAGRID_SPEECH = [
+    """
+    Hello, Muggle!
+    How fortunate, today you've got a chance to visit the world of Harry Potter!
+""",
+    """
+    Glad to see you again! 
+    Ready for new adventures?
+""",
+]
+
+OLLIVANDER_SPEECH = """
+    You are not a wizard without a wand!
+    Choose the one from the list that Mr.Ollivander has prepared for you:
+"""
+
+DUMBLEDORE_SPEECH = """
+    I see you've got the stuff. It remains to decide where you will study.
+    Put on the Sorting Hat!
+"""
+
+HARRY_SPEECH = """
+    Yo, buddy! Hurry up, the train will depart soon!
+    Once we get to a compartment, tell me how you spent your holidays...
+"""
+
+HELP_ = """
+    In each message, the bot gives you options for the course of the dialogue,
+    you just need to select one of them and confirm the transition by entering 
+    the corresponding code you could see in square brackets.
+    
+    [home] > start from beginning
+    [back] > take a step back to answer the question in a different way
+    
+    You may also need [back] if you receive a "fallback" notification 
+    - this may happen if you have entered an unknown command.
+"""
+
+NAVIGATOR = {
+    "start": "let it begin",
+    "help": "call for help",
+    "back": "go back",
+    "hat": "take the hat",
+    "next": "see you next year",
+}
+
+FACULTIES = {1: "Gryffindor", 2: "Hufflepuff", 3: "Ravenclaw", 4: "Slytherin"}
+
+WANDS = {
+    1: '11" long, made of holly, and possessed a phoenix feather core',
+    2: "Brand-new wand. Fourteen inches, willow, containing one unicorn tail-hair",
+    3: '10¾" long, made of vine wood, and possessed a dragon heartstring core.',
+}
