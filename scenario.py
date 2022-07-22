d2c81f17ee2305090c011d9008ce7c9fc70550ea
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-22 20:06:08 +0400

    chore: extend the scenario

--- scenario.py
+++ scenario.py
@@ -3,7 +3,7 @@ from colorama import Fore
 
 import df_engine.conditions as cnd
 import df_engine.labels as lbl
-from tools import sorting_hat, get_help, pickup_wand, get_response
+from tools import sorting_hat, get_help, pickup_wand, get_start_navi_hint, get_ollivanders_offer
 from df_engine.core.keywords import (
     TRANSITIONS,
     RESPONSE,
@@ -11,14 +11,17 @@ from df_engine.core.keywords import (
     PRE_RESPONSE_PROCESSING, PRE_TRANSITIONS_PROCESSING,
 )
 
-from vocabulary import greeting, ollivander
+from vocabulary import hagrids_greeting, ollivanders_speech
 
 script = {
     "root": {
         "start_node": {
-            RESPONSE: greeting,
+            PRE_RESPONSE_PROCESSING: {
+                "navi": get_start_navi_hint
+            },
+            RESPONSE: hagrids_greeting,
             TRANSITIONS: {
-                ("quest", "ollivanders"): cnd.exact_match("fwd"),
+                ("first_year", "ollivanders_shop"): cnd.exact_match("start"),
             },
         },
         "fallback_node": {RESPONSE: f"{Fore.MAGENTA} Avada Kedavra!"},
@@ -29,18 +32,21 @@ script = {
         },
         TRANSITIONS: {
             lbl.to_start(): cnd.regexp(r"home|start|hello|hi", re.I),
-            lbl.repeat(1.1): cnd.regexp(r"help", re.I),
-            lbl.previous(): cnd.regexp(r"back", re.I),
+            lbl.repeat(): cnd.exact_match(r"help", re.I),
+            lbl.previous(): cnd.exact_match(r"back", re.I),
         },
     },
-    "quest": {
-        "ollivanders": {
-            RESPONSE: ollivander,
+    "first_year": {
+        "ollivanders_shop": {
+            PRE_RESPONSE_PROCESSING: {
+                "navi": get_ollivanders_offer
+            },
+            RESPONSE: ollivanders_speech,
             PRE_TRANSITIONS_PROCESSING: {
                 "pickup_wand": pickup_wand,
             },
             TRANSITIONS: {
-                lbl.forward(): cnd.true()
+                lbl.forward(): cnd.regexp(r'1|2|3')
             },
         },
         "sorting_hat": {
@@ -49,8 +55,8 @@ script = {
             },
             RESPONSE: "Let the Sorting Hat decide where you will study!",
             TRANSITIONS: {
-                lbl.forward(): cnd.regexp(r"fwd", re.I),
-                lbl.repeat(): cnd.regexp(r"hat", re.I),
+                lbl.forward(): cnd.exact_match(r"fwd", re.I),
+                lbl.repeat(): cnd.exact_match(r"hat", re.I),
             },
         },
     },
