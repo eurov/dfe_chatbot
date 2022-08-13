eab844c75e4d5bc35e054030d0b8a4b018da1d6c
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-08-13 20:15:26 +0400

    feat: connect to ydb

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ logger = logging.getLogger(__name__)
 DB_NAME = os.getenv("PG_NAME")
 DB_HOST = os.getenv("PG_HOST")
 DB_PORT = os.getenv("PG_PORT")
-DB_LOGIN = os.getenv("PG_USERNAME")
-DB_PASSWORD = os.getenv("PG_PASSWORD")
+# DB_LOGIN = os.getenv("PG_USERNAME")
+# DB_PASSWORD = os.getenv("PG_PASSWORD")
 
-db = connector_factory(f"postgresql://{DB_LOGIN}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")  # try except?
+db = connector_factory(f"grpc://{DB_HOST}:{DB_PORT}/{DB_NAME}")  # try except?
 
 
 ACTOR = Actor(
